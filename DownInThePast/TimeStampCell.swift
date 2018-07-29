6abc3205f75655f78a58d6b022b5f0e3c437b3a7
Anton <anton@engomac5.local>
2018-07-29 15:54:28 +0200

    add TableView incl. CustomView

---
+++ DownInThePast/TimeStampCell.swift
@@ -0,0 +1,16 @@
+//
+//  TimeStampCell.swift
+//  DownInThePast
+//
+//  Created by Anton on 29.07.18.
+//  Copyright © 2018 Anton. All rights reserved.
+//
+
+import UIKit
+
+class TimeStampCell: UITableViewCell {
+
+    @IBOutlet weak var TimeStampLabel: UILabel!
+    
+
+}
--- DownInThePast/ViewController.swift
+++ DownInThePast/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var timeStampTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeStampTableView.dataSource = self
+        timeStampTableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK - UITableViewDataSource Stuff
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeStampCell", for: indexPath) as! TimeStampCell
+        
+        return cell
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 }
 
